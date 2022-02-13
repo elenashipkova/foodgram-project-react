25ae601d6188f7a51f20fa640653e7a3e0765111
Elena <eshipkova@iMac-Dmitry.local>
2022-02-13 14:05:28 +0300

    First deploy tests4

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,10 +1,10 @@
 from django.core.exceptions import ValidationError
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
 from recipes.models import (FavoritesList, Follow, Ingredient,
                             IngredientRecipe, Recipe, ShoppingList, Tag)
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 from users.models import User
 
 
