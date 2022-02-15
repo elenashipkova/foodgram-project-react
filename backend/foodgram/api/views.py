9077d2f5a3e04dc1e0fa18fed7f44eabdd5d51a0
Elena <eshipkova@iMac-Dmitry.local>
2022-02-15 23:44:43 +0300

    First deploy shopping_cart1

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -117,6 +117,6 @@ class DownloadShoppingList(APIView):
                 f'{item} ({shop_list[item]["measurement_unit"]}) - '
                 f'{shop_list[item]["amount"]} \n')
         filename = 'shopping_cart.txt'
-        response = HttpResponse(download_list, content_type='text/plain')
+        response = HttpResponse('ghjghjkk', content_type='text/plain')
         response['Content-Disposition'] = f'attachment; filename={filename}'
         return response
