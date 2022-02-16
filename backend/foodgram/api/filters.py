d0fb7482fcf19ea3c4d5130c4af96d1493faaf3e
Elena <eshipkova@iMac-Dmitry.local>
2022-02-16 09:05:23 +0300

    First deploy filters

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -14,6 +14,7 @@ class IngredientFilter(filters.FilterSet):
 
 
 class RecipeFilter(filters.FilterSet):
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
@@ -26,11 +27,15 @@ class RecipeFilter(filters.FilterSet):
 
     def get_is_favorited(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
-            return Recipe.objects.filter(favorites__user=self.request.user)
-        return Recipe.objects.all()
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
             return Recipe.objects.filter(
                 shopping_cart__user=self.request.user)
         return Recipe.objects.all()
+
+    class Meta:
+        model = Recipe
+        fields = ('author', 'tags')
